4bc4281eb1d203386e8babd68526839ffcde25e6
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2026-04-16 21:34:02 +0530

    structured analyse_video endpoint

--- app/routes/api.py
+++ app/routes/api.py
@@ -7,6 +7,7 @@ from app.schemas import AnalyseRequest
 from app.services.video_detector import predict_video
 from app.services.downloader import download_file
 import os
+import uuid
 
 router = APIRouter()
 
@@ -17,66 +18,49 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/analyse")
-def analyse(
+
+@router.post("/analyse/video-url")
+def analyse_video_url(
     request: AnalyseRequest,
     x_api_key: str = Header(...),
     db: Session = Depends(get_db)
 ):
-
     key = db.query(models.APIKey).filter(models.APIKey.key == x_api_key).first()
 
     if not key:
-        raise HTTPException(status_code=401, detail="Invalid API key")
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+    
+    try:
+        temp_path = download_file(request.input)
 
-    input_type = request.type
-    input_data = request.input
+        prediction = predict_video(temp_path)
 
-    if input_type == "image":
-        result = "fake"
-        confidence = "0.91"
+        result = prediction["prediction"]
+        confidence = str(prediction["confidence"])
 
-    elif input_type == "video":
-        try:
-            temp_path = "test.mp4"
-            
-            prediction = predict_video(temp_path)
+        scan = models.Scan(
+            user_id=key.user_id,
+            input_data=request.input,
+            result=result,
+            confidence=confidence
+        )
+        db.add(scan)
+        db.commit()
 
-            result = prediction["prediction"]
-            confidence = str(prediction["confidence"])
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
 
-            if os.path.exists(temp_path):
-                os.remove(temp_path)
+        return{
+            "type": "video-url",
+            "result": result,
+            "confidence": confidence
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
 
-    elif input_type == "audio":
-        result = "fake"
-        confidence = "0.82"
-
-    elif input_type == "url":
-        result = "fake"
-        confidence = "0.88"
-    else:
-        raise HTTPException(status_code=400, detail="Invalid Type")
-
-    scan = models.Scan(
-        user_id=key.user_id,
-        input_data=input_data,
-        result=result,
-        confidence=confidence
-    )
-    db.add(scan)
-    db.commit()
-
-    return {
-        "type": input_type,
-        "result": result,
-        "confidence": confidence
-    }
-
-@router.post("/analyse_upload")
+@router.post("/analyse/analyse_upload")
 async def analyse_upload(
     file: UploadFile = File(...),
     x_api_key: str = Header(...),
@@ -88,7 +72,7 @@ async def analyse_upload(
         raise HTTPException(status_code=401, detail="Invalid API key")
     
     try:
-        temp_path = f"temp_{file.filename}"
+        temp_path = f"temp_{uuid.uuid4()}.mp4"
 
         with open(temp_path, "wb") as f:
             f.write(await file.read())
@@ -117,6 +101,8 @@ async def analyse_upload(
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
 
 @router.get("/history")
 def get_history(
